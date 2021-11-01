12e82b0cf2915469cce5e0e7d15b0a52222c7a3e
MrLucio <mrlucio@users.noreply.github.com>
2021-11-01 18:45:25 +0100

    test: add test files

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -1,8 +1,17 @@
 from mobi import Mobi
 
+
 # Test the reader with Alice In Wonderland's mobi file
 # Available at: https://www.gutenberg.org/ebooks/11
-def test_reader():
+def test_book_1():
   reader = Mobi('./alice_in_wonderland.mobi')
+  output = reader.read()  # bytearray containing the decoded mobi file
+  reader.close()
+
+
+# Test the reader with Divine Comedy's mobi file
+# Available at: https://www.gutenberg.org/ebooks/8799
+def test_book_2():
+  reader = Mobi('./divine_comedy.mobi')
   output = reader.read()  # bytearray containing the decoded mobi file
   reader.close()
